fdae25f5975b0997850881fbbd50c937064cf9e6
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-14 21:33:13 +0530

    feat: create and link article list

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -7,7 +7,7 @@ const Article = () => {
     if (!article) return <h1>Article does not exist!</h1>;
 
     return (
-        <div className="w-[720px] m-auto flex flex-col items-start py-10">
+        <div className="max-w-[720px] m-auto flex flex-col items-start py-10">
             <h1 className="font-bold text-3xl pb-6">{article.title}</h1>
             {
                 article.content.map((paragraph, key) => (
