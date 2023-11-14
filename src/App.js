e0e429728615fbaf2d4aec9cf807b8c37139c0a5
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-14 20:23:55 +0530

    feat: add routing of the pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticlesList from './pages/ArticlesList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ink Link</h1>
-      <div>
-
+    <Router>
+      <div className="App">
+        <h1>Ink Link</h1>
+        <div>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/articles" element={<ArticlesList />} />
+            <Route path="/articles/:articleId" element={<Article />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,7 @@
+const ArticlesList = () => {
+    return (
+        <h1>Articles</h1>
+    );
+}
+
+export default ArticlesList;
