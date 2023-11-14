4f53b5175a9d26e39230a04f1577d698b5b03b55
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-14 21:14:19 +0530

    feat: article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,19 @@
+import { useParams } from "react-router-dom";
+import articles from "../constants/article-content";
+
 const Article = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+    if (!article) return <h1>Article does not exist!</h1>;
+
     return (
-        <div>
-            <h1>Article</h1>
+        <div className="w-[720px] m-auto flex flex-col items-start py-10">
+            <h1 className="font-bold text-3xl pb-6">{article.title}</h1>
+            {
+                article.content.map((paragraph, key) => (
+                    <p className="text-justify font-medium mb-4" key={key}>{paragraph}</p>
+                ))
+            }
         </div>
     );
 }
