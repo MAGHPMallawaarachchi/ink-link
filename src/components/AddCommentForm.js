cd78a56eb6a3fed43bbc188bb82432e77c634c5b
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-16 00:49:50 +0530

    Add user authentication to comment form and upvote button

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import axios from "axios";
+import useUser from "../hooks/useUser";
 
 const AddCommentForm = ({ articleName, onArticleUpdated }) => {
     const [name, setName] = useState("");
     const [commentText, setCommentText] = useState("");
+    const { user } = useUser();
 
     const addComment = async () => {
-
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
         const response = await axios.post(`/api/articles/${articleName}/comments`,{
             username: name,
             text: commentText,
+        },{
+            headers,
         });
 
         const updatedArticle = response.data;
@@ -22,24 +27,13 @@ const AddCommentForm = ({ articleName, onArticleUpdated }) => {
     return(
         <div className="flex flex-col items-start w-full gap-2 pb-6 border-b-2 border-black">
             <h3 className="mb-2 text-xl font-bold text-left">Add a Comment</h3>
+            {user && <p className="pb-2 text-base font-medium text-left">You are commenting as {user.email}</p>}
 
-            <label className="flex flex-col items-start mb-2 text-base font-medium text-left">
-                Name
-                <input 
-                    className="w-[300px] px-2 py-1 border-2 border-black rounded-md"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    type="text"/>
-            </label>
-
-            <label className="flex flex-col items-start mb-2 text-base font-medium text-left">
-                Comment
-                <textarea 
-                    className="w-[300px] px-2 py-1 border-2 border-black rounded-md"
-                    value={commentText}
-                    onChange={e => setCommentText(e.target.value)}
-                    rows="4" cols="50"/>
-            </label>
+            <textarea 
+                className="w-[300px] px-2 py-1 border-2 border-black rounded-md mb-2"
+                value={commentText}
+                onChange={e => setCommentText(e.target.value)}
+                rows="4" cols="50"/>
 
             <button className="w-[300px] px-2 py-1 text-white bg-black rounded-md" onClick={addComment}>Add Comment</button>
         </div>
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -4,32 +4,41 @@ import articles from "../constants/article-content";
 import NotFound from "./NotFound";
 import axios from "axios";
 import CommentsList from "../components/CommentsList";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import AddCommentForm from "../components/AddCommentForm";
 import useUser from "../hooks/useUser";
+import { Link } from "react-router-dom";
 
 const Article = () => {
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [], canUpvote: false });
+    const { canUpvote } = articleInfo;
     const { articleId } = useParams();
 
     const { user, isLoading } = useUser();
 
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+        const loadArticleInfo = async () => {
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken: token } : {};
+            const response = await axios.get(`/api/articles/${articleId}`, { headers });
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
-        fetchData();
-    }, [articleId]);
+
+        if (isLoading) {
+            loadArticleInfo();
+        }
+    }, [isLoading, user, articleId]);
 
     const article = articles.find(article => article.name === articleId);
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
-    };
+    }
     
     if (!article) return <NotFound/>;
 
@@ -38,8 +47,8 @@ const Article = () => {
             <h1 className="pb-6 text-3xl font-bold">{article.title}</h1>
             <div className="flex flex-row items-center gap-1 mb-4">
                 {user
-                    ? <button onClick={addUpvote}><FaHeart/></button>
-                    : <FaHeart/>
+                    ? <button onClick={addUpvote}>{canUpvote ? <FaRegHeart/> : <FaHeart/>}</button>
+                    : <FaRegHeart/>
                 }
                 <h4 className="text-lg font-bold">{articleInfo.upvotes}</h4>
             </div>
@@ -49,7 +58,10 @@ const Article = () => {
                 ))
             }
             <div className="h-8"></div>
-            <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
+            {user
+                ? <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
+                : <Link to="/login" className="px-2 py-1 text-white bg-black rounded-md">Login to Comment</Link>
+            }
             <div className="h-6"></div>
             <CommentsList comments={articleInfo.comments}/>
         </div>
