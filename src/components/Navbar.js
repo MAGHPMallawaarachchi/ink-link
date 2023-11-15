9f88fc6c48fbb0444444750c0d40f4d0fc907f0f
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-16 01:00:37 +0530

    Add user authentication to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
 import { Link } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+import useUser from '../hooks/useUser';
 
 const Navbar = () => {
-    return(
-        <nav className="border-b-4 border-black">
-            <ul className='flex items-center justify-center text-base font-bold'>
+    const { user } = useUser();
+
+    return (
+        <nav className="flex items-center justify-between px-5 border-b-4 border-black">
+            <ul className='flex items-center justify-center flex-grow text-base font-bold'>
                 <li className="flex items-center justify-center h-10 hover:bg-black hover:text-white w-28">
                     <Link to="/">Home</Link>
                 </li>
-                <li  className="flex items-center justify-center h-10 hover:bg-black hover:text-white w-28">
+                <li className="flex items-center justify-center h-10 hover:bg-black hover:text-white w-28">
                     <Link to="/about">About</Link>
                 </li>
                 <li className="flex items-center justify-center h-10 hover:bg-black hover:text-white w-28">
                     <Link to="/articles">Articles</Link>
                 </li>
             </ul>
+            <div className='flex items-end'>
+                {user
+                    ? <button onClick={()=>{signOut(getAuth());}} className='px-3 py-1 text-white bg-black rounded-md'>Logout</button>
+                    : <Link className='px-3 py-1 text-white bg-black rounded-md' to="/login">Login</Link>
+                }
+            </div>
         </nav>
     );
 }
